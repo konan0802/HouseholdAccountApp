d61aeb13153ea38069d1ec95f5868b8c290f7302
konan0802 <46038618+konan0802@users.noreply.github.com>
2021-05-13 23:11:26 +0900

    add: operator

---
+++ adapter/controller/budget_operator.go
@@ -0,0 +1,5 @@
+package adapter
+
+// BudgetOperator 予実を操作する構造体
+type BudgetOperator struct {
+}
---
+++ adapter/controller/receipt_operator.go
@@ -0,0 +1,5 @@
+package adapter
+
+// ReceiptOperator レシートを操作する構造体
+type ReceiptOperator struct {
+}
